897c0db08c9bca1b497d8b44f7b8c58bf877e128
ゆせん <1405481963@qq.com>
2021-02-12 21:37:01 -0600

    Create merge-st.sh

---
+++ merge-st.sh
@@ -0,0 +1,10 @@
+git config --global user.email "1405481963@qq.com"
+git config --global user.name "Little-W"
+git config --global user.password "Wyx200304060292"
+git config --global credential.helper store
+cp -v /re/.git-credentials ~/
+git clone https://github.com/Little-W/android_kernel_xiaomi_ginkgo  --branch MIUI-R  /1
+cd /1
+git remote add st https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo
+git fetch st
+git merge st/MIUI-R
