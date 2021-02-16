57bce37630c49913c8f363178708eaddafdec8f5
ゆせん <1405481963@qq.com>
2021-02-16 17:07:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,8 +26,8 @@ function do_deps() {
         zlib1g-dev 
 }
 do_deps
-export PATH="~/tc/bin:$PATH"
-export LD_LIBRARY_PATH="~/tc/lib:$LD_LIBRARY_PATH"
+export PATH=~/tc/bin:$PATH
+export LD_LIBRARY_PATH=~/tc/lib:$LD_LIBRARY_PATH
 export KBUILD_BUILD_HOST="Litttle-W <1405481963@qq.com>"
 git config --global user.email "1405481963@qq.com"
 git config --global user.name "Little-W"
