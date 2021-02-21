76bbbb0782279ec862480de4875716b8f6c15031
ゆせん <1405481963@qq.com>
2021-02-21 08:31:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@ git config --global user.password "Wyx200304060292"
 git clone https://github.com/Little-W/redentials.git ~/re
 git config --global credential.helper store
 cp -v ~/re/.git-credentials ~/
-SECONDS=0
-ZIPNAME="Akari-ginkgo-$(date '+%Y%m%d-%H%M').zip"
 mkdir -p out
 make O=out ARCH=arm64 vendor/ginkgo-perf_defconfig
 if [[ $1 == "-r" || $1 == "--regen" ]]; then
@@ -33,31 +31,4 @@ make 	  -j$(nproc --all)  O=out \
                           Image.gz-dtb \
                           dtbo.img
 fi
-
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
-git clone https://github.com/Little-W/AnyKernel3.git
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
-rm -f *zip
-cd AnyKernel3
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-cd ..
-rm -rf AnyKernel3
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-if command -v gdrive &> /dev/null; then
-gdrive upload --share $ZIPNAME
-else
-echo "Zip: $ZIPNAME"
-fi
-rm -rf out
-else
-echo -e "\nCompilation failed!"
-fi
-git clone https://github.com/Little-W/release.git ~/release
-cp -v $ZIPNAME ~/release
-cd ~/release
-git add .
-git commit -m "$ZIPNAME"
-git push -f
 
