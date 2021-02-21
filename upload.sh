12b2fd8c607fbfa2d37d3fa4d393379222a43264
ゆせん <1405481963@qq.com>
2021-02-21 09:01:14 -0600

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,10 +1,6 @@
-git config --global user.email "1405481963@qq.com"
-git config --global user.name "Little-W"
-git config --global user.password "Wyx200304060292"
-
 SECONDS=0
 ZIPNAME="Akari-ginkgo-$(date '+%Y%m%d-%H%M').zip"
-#if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
 git clone https://github.com/Little-W/AnyKernel3.git
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
@@ -20,13 +16,13 @@ gdrive upload --share $ZIPNAME
 else
 echo "Zip: $ZIPNAME"
 fi
-#else
-#echo -e "\nCompilation failed!"
-#fi
-git clone https://github.com/Little-W/release.git ~/release
+else
+echo -e "\nCompilation failed!"
+fi
+git clone --depth 1 https://github.com/Little-W/release.git ~/release
 cp -v $ZIPNAME ~/release
 cd ~/release
 git add .
 git commit -m "$ZIPNAME"
-git push -f
+git push 
 
