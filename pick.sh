1008b9b55afcde928bdf4d0d214444b1e2d1a6e7
ゆせん <1405481963@qq.com>
2021-02-22 21:44:26 +0800

    Update pick.sh

--- pick.sh
+++ pick.sh
@@ -4,9 +4,9 @@ git config --global user.password "Wyx200304060292"
 git config --global credential.helper store
 git clone https://github.com/Little-W/redentials.git /re
 cp -v /re/.git-credentials ~/
-git clone https://github.com/Little-W/android_kernel_xiaomi_ginkgo   /1
+git clone https://github.com/Little-W/clang.git   /1
 cd /1
 #git branch -m f739fd5bf17ee9b0a8b60abd8295a71deda0fad4 MIUI-R
-git checkout f1f07a36e9c6a58a521b4c5fe880bd13ec6c8d77
+git rest --hard  9cd6b429ccc5f172ba4a5ac34ad2ed84fbb1f783
 #git cherry-pick e35e86812a7e35c1320fc51c45565f61478dd045^..
 git push -f
