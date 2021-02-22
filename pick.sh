ef2562af5451d9b1ceb33c72fd92d787762a3d71
ゆせん <1405481963@qq.com>
2021-02-22 21:15:33 +0800

    Update pick.sh

--- pick.sh
+++ pick.sh
@@ -7,6 +7,6 @@ cp -v /re/.git-credentials ~/
 git clone https://github.com/Little-W/android_kernel_xiaomi_ginkgo   /1
 cd /1
 #git branch -m f739fd5bf17ee9b0a8b60abd8295a71deda0fad4 MIUI-R
-git reset --hard f1f07a36e9c6a58a521b4c5fe880bd13ec6c8d77
+git checkout f1f07a36e9c6a58a521b4c5fe880bd13ec6c8d77
 #git cherry-pick e35e86812a7e35c1320fc51c45565f61478dd045^..
 git push -f
