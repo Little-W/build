4964760054670a3ea0fd469328cfac9bd80a5865
ゆせん <1405481963@qq.com>
2021-03-22 23:37:14 -0500

    Update merge-st.sh

--- merge-st.sh
+++ merge-st.sh
@@ -6,8 +6,12 @@ git clone https://github.com/Little-W/redentials.git /re
 cp -v /re/.git-credentials ~/
 git clone https://github.com/Little-W/android_kernel_xiaomi_ginkgo  --branch MIUI-R  /1
 cd /1
-git remote add st https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo
-git fetch st
-git merge st/MiuiR -X theirs --no-commit
-git commit -m "merge upstream branch"
+git remote add sl https://github.com/ghostrider-reborn/android_kernel_xiaomi_ginkgo
+git fetch sl
+git checkout sl/11.0
+git checkout -b AOSP-R
+#git remote add st https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo
+#git fetch st
+#git merge st/MiuiR -X theirs --no-commit
+#git commit -m "merge upstream branch"
 git push
