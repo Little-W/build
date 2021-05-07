bdc562a91419a841d3912c6b7bb4d61c77255712
ゆせん <1405481963@qq.com>
2021-05-07 09:06:41 -0500

    Update pick.sh

--- pick.sh
+++ pick.sh
@@ -4,7 +4,7 @@ git config --global user.password "Wyx200304060292"
 git config --global credential.helper store
 git clone git@github.com:Little-W/redentials.git /re
 cp -v /re/.git-credentials ~/
-git clone  https://github.com/Little-W/proton-clang/commits/master /1
+git clone  https://github.com/Little-W/proton-clang /1
 cd /1
 #git branch -m f739fd5bf17ee9b0a8b60abd8295a71deda0fad4 MIUI-R
 git reset --hard  08a35cd973e866932ca1b7f3e573293e0829c129
