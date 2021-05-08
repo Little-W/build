dfac1f495f5c3ecfc9cf0cb7d096b4e49d359b36
ゆせん <1405481963@qq.com>
2021-05-08 09:32:28 -0500

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -23,13 +23,16 @@ sudo apt-get install -y --no-install-recommends \
         zip \
         xz-utils \
 	llvm \
+        clang \
         zlib1g-dev 
+
 sudo ln -s /usr/include/asm-generic /usr/include/asm
 git clone https://github.com/bminor/glibc --depth 1 ~/glibc
 cd ~/glibc
 mkdir build
 cd build
 ../configure --prefix=/usr --disable-profile --enable-add-ons --with-headers=/usr/include --with-binutils=/usr/bin
+make headers_install
 make -j8
 sudo make install
 git clone --depth 1 https://github.com/Klozz/Yuki-clang ~/tc
