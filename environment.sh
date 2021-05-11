948c2ac162898b42d0eb4a5cce57dfb52ccbcb92
ゆせん <1405481963@qq.com>
2021-05-11 10:54:05 +0800

    Create environment.sh

--- environment.sh
+++ environment.sh
@@ -28,20 +28,20 @@ sudo apt-get install -y --no-install-recommends \
         xz-utils \
         zlib1g-dev 
 
-sudo wget -P /home/ https://github.com/sgerrand/docker-glibc-builder/releases/download/2.33-0/glibc-bin-2.33-0-x86_64.tar.gz
-cd /home/
-sudo gzip -d glibc-bin-2.33-0-x86_64.tar.gz
-sudo tar -xvf glibc-bin-2.33-0-x86_64.tar
-ls /lib/x86_64-linux-gnu/
-echo "-----------"
+#sudo wget -P /home/ https://github.com/sgerrand/docker-glibc-builder/releases/download/2.33-0/glibc-bin-2.33-0-x86_64.tar.gz
+#cd /home/
+#sudo gzip -d glibc-bin-2.33-0-x86_64.tar.gz
+#sudo tar -xvf glibc-bin-2.33-0-x86_64.tar
+#ls /lib/x86_64-linux-gnu/
+#echo "-----------"
 #ls /lib
 #export LD_PRELOAD=/home/glibc-bin-2.33-0-x86_64/usr/glibc-compat\lib\libc-2.33.so
-export LD_LIBRARY_PATH=/home/glibc-bin-2.33-0-x86_64/usr/glibc-compat\lib
-sudo rm -rf /lib/x86_64-linux-gnu/libc.so.6
-LD_PRELOAD=/lib/x86_64-linux-gnu/libc-2.31.so sudo ln -s /home/glibc-bin-2.33-0-x86_64/usr/glibc-compat\lib\libc-2.33.so  /lib/x86_64-linux-gnu/libc.so.6
-git clone --depth 1 https://github.com/kutemeikito/RastaMod69-Clang ~/tc
+#export LD_LIBRARY_PATH=/home/glibc-bin-2.33-0-x86_64/usr/glibc-compat\lib
+#sudo rm -rf /lib/x86_64-linux-gnu/libc.so.6
+#LD_PRELOAD=/lib/x86_64-linux-gnu/libc-2.31.so sudo ln -s /home/glibc-bin-2.33-0-x86_64/usr/glibc-compat\lib\libc-2.33.so  /lib/x86_64-linux-gnu/libc.so.6
+git clone --depth 1 https://github.com/kdrag0n/proton-clang ~/tc
 ~/tc/bin/clang -v
 sudo dd if=/dev/zero of=/swapfile bs=1M count=40960
 sudo mkswap /swapfile
 sudo swapon /swapfile
-cd /home/circleci/project
+#cd /home/circleci/project
