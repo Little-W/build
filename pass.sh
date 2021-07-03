9510815a169e547f0a630a685604a97293264f57
ゆせん <1405481963@qq.com>
2021-07-03 13:19:54 +0800

    Create pass.sh

---
+++ pass.sh
@@ -0,0 +1,15 @@
+	mkdir -p out
+  make O=out ARCH=arm64 vendor/alioth_defconfig
+	echo -e "\n" | make   -j$(nproc --all)  O=$BASE/out \
+                          ARCH=arm64 \
+                          CC="clang" \
+                          AR="llvm-ar" \
+                          NM="llvm-nm" \
+					             LD="ld.lld" \
+			              AS="llvm-as" \
+						      STRIP="llvm-strip" \
+			              OBJCOPY="llvm-objcopy" \
+			              OBJDUMP="llvm-objdump" \
+					    	  CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE=aarch64-linux-gnu-  \
+                          CROSS_COMPILE_ARM32=arm-linux-gnueabi- 
