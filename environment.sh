3d0ff1b4d1e768d388a9ec98dd696dd58a46bc87
ゆせん <1405481963@qq.com>
2021-07-03 16:28:41 +0800

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
+sudo apt update
+sudo apt install -y --no-install-recommends \
         bc \
         bison \
         ca-certificates \
