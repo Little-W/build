d3c67a3c41cdcf1cebb69b0523e2a4559576202a
ゆせん <1405481963@qq.com>
2021-07-05 12:39:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,19 +3,31 @@ export LD_LIBRARY_PATH=~/tc/lib:$LD_LIBRARY_PATH
 export KBUILD_BUILD_HOST="Litttle-W <1405481963@qq.com>"
 export KBUILD_BUILD_USER="Akari Kernel Project"
 mkdir -p out
-cp arch/arm64/configs/vendor/alioth_stock_defconfig out/.config
+make                      ARCH=arm64 \
+                          CC="clang" \
+                          AR="llvm-ar" \
+                          NM="llvm-nm" \
+		          LD="ld.lld" \
+			  AS="llvm-as" \
+			  STRIP="llvm-strip" \
+			  OBJCOPY="llvm-objcopy" \
+			  OBJDUMP="llvm-objdump" \
+			  CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE=aarch64-linux-gnu-  \
+                          CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+			  O=out  alioth_defconfig
 echo -e "\nStarting compilation...\n"
 make 	  -j$(nproc --all)  O=out \
                           ARCH=arm64 \
                           CC="clang" \
                           AR="llvm-ar" \
                           NM="llvm-nm" \
-					      LD="ld.lld" \
-			              AS="llvm-as" \
-						  STRIP="llvm-strip" \
-			              OBJCOPY="llvm-objcopy" \
-			              OBJDUMP="llvm-objdump" \
-						  CLANG_TRIPLE=aarch64-linux-gnu- \
+			  LD="ld.lld" \
+			  AS="llvm-as" \
+			  STRIP="llvm-strip" \
+			  OBJCOPY="llvm-objcopy" \
+			  OBJDUMP="llvm-objdump" \
+			  CLANG_TRIPLE=aarch64-linux-gnu- \
                           CROSS_COMPILE=aarch64-linux-gnu-  \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                           Image.gz-dtb \
